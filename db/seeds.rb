f6ca7d494932bfdd0755dc6e1684299b3d48df87
Alex Fedoseev <alex.fedoseev@gmail.com>
2015-07-20 14:50:41 +0300

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'demo@demo.me', password: 'demopass')
+
+Comment.create([
+                   { author: 'Alex', comment: 'Yay! It works!' },
+                   { author: 'Guest', comment: 'True! Isomorphic awesomeness!' }
+              ])
